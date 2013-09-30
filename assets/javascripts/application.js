ae0ba19bec7c97327b1904259d7c3294f83bd2fa
Arnelle Balane <arnellebalane@gmail.com>
2013-09-30 11:55:02 +0800

    show flash notifications

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
   item.initialize();
+  notifications.initialize();
 });
 
 var item = {
   initialize: function() {
     var height = Math.max($(".item .main").outerHeight(), $(".item .sidebar").outerHeight());
     $(".item .main, .item .sidebar").outerHeight(height);
+  }
+};
+
+var notifications = {
+  initialize: function() {
+    if ($("p.notification").length > 0) {
+      notifications.reveal($("p.notification").first());
+    }
+  },
+  reveal: function(notification) {
+    notification.css({"margin-left": -(notification.outerWidth() / 2) + "px"}).addClass("shown");
+    setTimeout(function() {
+      notification.removeClass("shown");
+    }, 2500);
   }
 };
